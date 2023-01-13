9405e6ce81dcc89609d59c430d1708c520e19173
RoyBuchanandev <93665139+RoyBuchanandev@users.noreply.github.com>
2023-01-13 16:26:54 -0300

    Add files via upload

--- desafio/app.js
+++ desafio/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 8086;
+const port = 8087;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
--- desafio/routes/carts.js
+++ desafio/routes/carts.js
@@ -1,20 +1,99 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const fse = require('fs-extra');
 
-router.get('/', (req, res) => {
-    res.send('carts');
-});
+let carts = [];
+
+let products = JSON.parse(fs.readFileSync('products.txt', 'utf8')) || [];
 
 router.post('/', (req, res) => {
-    res.send('Add cart');
+    try {
+        let cart = {
+            id: carts.length + 1,
+            products: []
+        };
+        carts.push(cart);
+        fs.writeFileSync('carrito.txt', JSON.stringify(carts));
+        res.json({ message: 'Cart added', cart });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Error adding cart' });
+    }
 });
 
-router.put('/:id', (req, res) => {
-    res.send(`Update cart ${req.params.id}`);
+router.get('/:cid', (req, res) => {
+    let cart = carts.find(c => c.id === parseInt(req.params.cid));
+    if (!cart) {
+        res.status(404).json({ message: 'Cart not found' });
+        return;
+    }
+
+    let cartProducts = cart.products.map(p => {
+        let product = products.find(pr => pr.id === p.id);
+        return {
+            id: product.id,
+            name: product.name,
+            price: product.price,
+            quantity: p.quantity
+        }
+    });
+
+    res.json({ cart: cartProducts });
 });
 
-router.delete('/:id', (req, res) => {
-    res.send(`Delete cart ${req.params.id}`);
+router.post('/:cid/product/:pid', (req, res) => {
+    try {
+        let cart = carts.find(c => c.id === parseInt(req.params.cid));
+        if (!cart) {
+            res.status(404).json({ message: 'Cart not found' });
+            return;
+        }
+        let productIndex = cart.products.findIndex(p => p.id === parseInt(req.params.pid));
+        if (productIndex === -1) {
+            cart.products.push({ id: parseInt(req.params.pid), quantity: 1 });
+        } else {
+            cart.products[productIndex].quantity++;
+        }
+        fs.writeFileSync('carrito.txt', JSON.stringify(carts));
+        res.json({ message: 'Product added to cart', cart });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Error adding product to cart' });
+    }
 });
+
+router.put('/:cid', (req, res) => {
+    try {
+        let cart = carts.find(c => c.id === parseInt(req.params.cid));
+        if (!cart) {
+            res.status(404).json({ message: 'Cart not found' });
+            return;
+        }
+        cart.products = req.body.products;
+        fs.writeFileSync('carrito.txt', JSON.stringify(carts));
+        res.json({ message: 'Cart updated', cart });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Error updating cart' });
+    }
+});
+
+router.delete('/:cid', (req, res) => {
+    try {
+        let cartIndex = carts.findIndex(c => c.id === parseInt(req.params.cid));
+        if (cartIndex === -1) {
+            res.status(404).json({ message: 'Cart not found' });
+            return;
+        }
+        carts.splice(cartIndex, 1);
+        fs.writeFileSync('carrito.txt', JSON.stringify(carts));
+        res.json({ message: 'Cart deleted' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Error deleting cart' });
+    }
+});
+
 
 module.exports = router;
